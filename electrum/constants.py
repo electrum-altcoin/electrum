b1adea1fc5cf1de5a02f38d0a5577bf2ee73fcef
Mark Brooker <mark@walkjivefly.com>
2020-06-28 10:41:36 +0200

    Set Crown as default network

--- electrum/constants.py
+++ electrum/constants.py
@@ -40,12 +40,12 @@ networks = {
     'Namecoin-Mainnet': NamecoinMainnet,
 }
 
-net = networks['Bitcoin']
+net = networks['Crowncoin']
 
 def select_network(network='Bitcoin'):
     if not network in networks:
         raise Exception('Invalid Network. Available: {}'.format(
             list(networks.keys())))
     global net
-    net = networks.get(network, 'Bitcoin')
+    net = networks.get(network, 'Crowncoin')
     return
